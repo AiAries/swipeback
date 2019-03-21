e6fba332cd8b6817101bbabc88e93989d80c138e
azp <940066717@qq.com>
2019-03-21 11:13:59 +0800

    添加swipeback组件具体代码

---
+++ swipeback/src/main/java/com/azp/swipeback/SwipeBackActivityHelper.java
@@ -0,0 +1,62 @@
+package com.azp.swipeback;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+
+
+/**
+ * @author Yrom
+ */
+public class SwipeBackActivityHelper {
+    private Activity mActivity;
+
+    private SwipeBackLayout mSwipeBackLayout;
+
+    public SwipeBackActivityHelper(Activity activity) {
+        mActivity = activity;
+    }
+
+    @SuppressWarnings("deprecation")
+    public void onActivityCreate() {
+        mActivity.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mActivity.getWindow().getDecorView().setBackgroundDrawable(null);
+        mSwipeBackLayout = (SwipeBackLayout) LayoutInflater.from(mActivity).inflate(
+                R.layout.swipeback_layout, null);
+        mSwipeBackLayout.addSwipeListener(new SwipeBackLayout.SwipeListener() {
+            @Override
+            public void onScrollStateChange(int state, float scrollPercent) {
+                System.out.println(state);
+                System.out.println(scrollPercent);
+            }
+
+            @Override
+            public void onEdgeTouch(int edgeFlag) {
+                System.out.println("onEdgeTouch");
+                Utils.convertActivityToTranslucent(mActivity);
+            }
+
+            @Override
+            public void onScrollOverThreshold() {
+                System.out.println("onScrollOverThreshold");
+            }
+        });
+    }
+
+    public void onPostCreate() {
+        mSwipeBackLayout.attachToActivity(mActivity);
+    }
+
+    public View findViewById(int id) {
+        if (mSwipeBackLayout != null) {
+            return mSwipeBackLayout.findViewById(id);
+        }
+        return null;
+    }
+
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mSwipeBackLayout;
+    }
+}
